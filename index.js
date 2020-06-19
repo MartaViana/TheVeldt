0e0fb1aa2986d2c2cc3bc3b56123702ceeca3cc7
Marta Viana <1112marta@gmail.com>
2020-06-19 20:09:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,155 @@
+$(document).ready(function() {
+    var $magic = $(".magic"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+$(document).ready(function() {
+    var $magic = $(".magics"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+$(document).ready(function() {
+    var $magic = $(".magic_c"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+
+
+$(document).ready(function() {
+    var $magic = $(".magic_codigo"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+
+$(document).ready(function() {
+    var $magic = $(".magic_nota"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+
+$(document).ready(function() {
+    var $magic = $(".magic_chaves"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+$(document).ready(function() {
+    var $magic = $(".magic_q"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+$(document).ready(function() {
+    var $magic = $(".magic_porta"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+$(document).ready(function() {
+    var $magic = $(".magic_calen"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+$(document).ready(function() {
+    var $magic = $(".magic_cart"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+$(document).ready(function() {
+    var $magic = $(".magic_livro"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+$(document).ready(function() {
+    var $magic = $(".magic_sala"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+$(document).ready(function() {
+    var $magic = $(".magic_pc"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+
+$(document).ready(function() {
+    var $magic = $(".magic_quadro"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+$(document).ready(function() {
+    var $magic = $(".magic_diario"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
+
+$(document).ready(function() {
+    var $magic = $(".magic_caixa"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+
+        $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+});
